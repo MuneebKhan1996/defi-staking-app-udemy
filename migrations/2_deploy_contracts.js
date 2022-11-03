2782fb22e8da5816d9696c8041236352444823f5
MuneebKhan1996 <rmkhan1996@hotmail.com>
2022-11-03 23:43:22 +0500

    Contracts done

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,23 @@
 const Tether = artifacts.require("Tether");
+const RWD = artifacts.require("RWD");
+const DecentralBank = artifacts.require("DecentralBank");
 
-module.exports = async function(deployer) {
+module.exports = async function(deployer, network, accounts) {
+  // Deploy Mock Tether Contract
   await deployer.deploy(Tether);
+  const tether = await Tether.deployed();
+
+  // Deploy Mock RWD Contract
+  await deployer.deploy(RWD);
+  const rwd = await RWD.deployed();
+
+  // Deploy Mock DecentralBank Contract
+  await deployer.deploy(DecentralBank, rwd.address, tether.address);
+  const decentralBank = await DecentralBank.deployed();
+
+  //Transfer RWD tokens to decentral bank
+  await rwd.transfer(decentralBank.address, "1000000000000000000000000");
+
+  //Distribute 100 Tokens to investor
+  await tether.transfer(accounts[1], "100000000000000000000");
 };
--- src/contracts/RWD.sol
+++ src/contracts/RWD.sol
@@ -1,9 +1,9 @@
 pragma solidity ^0.5.0;
 
-contract Tether {
-    string public name = "Mock Tether Token";
-    string public symbol = "mUSDT";
-    uint256 public totalSupply = 1000000000000000000000000; // 1 million tokens
+contract RWD {
+    string public name = "Reward Token";
+    string public symbol = "RWD";
+    uint256 public totalSupply = 1000000000000000000000000; //one million tokens
     uint8 public decimals = 18;
 
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
@@ -25,13 +25,13 @@ contract Tether {
         public
         returns (bool success)
     {
-        // require that the value is greater or equal for transfer
         require(balanceOf[msg.sender] >= _value);
-        // transfer the amount and subtract the balance
+
         balanceOf[msg.sender] -= _value;
-        // add the balance
         balanceOf[_to] += _value;
+
         emit Transfer(msg.sender, _to, _value);
+
         return true;
     }
 
@@ -41,6 +41,7 @@ contract Tether {
     {
         allowance[msg.sender][_spender] = _value;
         emit Approval(msg.sender, _spender, _value);
+
         return true;
     }
 
@@ -51,12 +52,13 @@ contract Tether {
     ) public returns (bool success) {
         require(_value <= balanceOf[_from]);
         require(_value <= allowance[_from][msg.sender]);
-        // add the balance for transferFrom
-        balanceOf[_to] += _value;
-        // subtract the balance for transferFrom
-        balanceOf[_from] -= _value;
+
+        balanceOf[msg.sender] += _value;
+        balanceOf[_to] -= _value;
+
         allowance[msg.sender][_from] -= _value;
         emit Transfer(_from, _to, _value);
+
         return true;
     }
 }
